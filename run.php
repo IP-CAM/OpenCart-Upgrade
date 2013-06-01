93479336f8c4b1f05354d080a0392c17af410ee3
Jim Lind <spoon.vw@gmail.com>
2013-06-01 15:52:20 -0500

    Dump Address Ids as a Hello World

--- run.php
+++ run.php
@@ -1,7 +1,13 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+require_once '../config.php';
+$pdo = new PDO('mysql:host='.DB_HOSTNAME.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+
+$rows = $pdo->query('SELECT * FROM address');
+
+foreach($rows as $row) {
+    echo $row['address_id'];
+	echo ' : ';
+}
