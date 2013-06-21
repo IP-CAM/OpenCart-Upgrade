c90016c870ac6bae815be44b517da783312974c0
Jim Lind <spoon.vw@gmail.com>
2013-06-20 22:20:05 -0500

    Path Update
    Pretty sure this isn't neccesary.

--- run.php
+++ run.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require_once '../config.php';
-require_once './db.php';
+require_once 'db.php';
 
 echo '<b>TODO:</b> Move Category Images Around.';
 echo '<br />';
